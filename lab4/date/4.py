a491b0b5d7ce829988364b99e4020c60ad31530b
Ademi Sapakova <a_sapakova@kbtu.kz>
2022-03-04 19:54:00 +0600

    lab4

--- lab4/date/4.py
+++ lab4/date/4.py
@@ -5,4 +5,4 @@ from datetime import datetime, timedelta
 date1 = datetime.now()
 date2 = datetime(2021, 6, 1, 23, 30, 30)
 difference = date1 - date2
-print(difference.total_seconds())
+print(difference.total_seconds())  
